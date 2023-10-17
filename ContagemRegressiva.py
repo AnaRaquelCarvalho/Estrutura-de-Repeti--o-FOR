5f1442a07d04fa9d2f1276b240cd1df8d0b865ce
Ana R. Carvalho <137975286+AnaRaquelCarvalho@users.noreply.github.com>
2023-10-17 18:00:28 -0300

    Create ContagemRegressiva.py
    Contaqgem Regressiva

---
+++ ContagemRegressiva.py
@@ -0,0 +1,8 @@
+print('='*3, 'Contagem Regressiva - Fogos de artifício' ,'='*3)
+
+from time import sleep
+for c in range (10,0, -1):
+    print('        ⏳⏳⏳⏳⏳    ',c,'      ⏳⏳⏳⏳⏳')
+    sleep(1)
+print('FELIZ ANO NOVO!!!✨✨🎇🎇🎉🎉🎉 \nFELIZ 2024!!!','✨✨🎇🎇🎉🎉🎉🥂🥂🥂')
+print('BUM!!! BUM !!!✨✨\n Bum!!! Bum!!! Bum!!!✨✨🎇🎇\n Tcham!!!  Tcham!!!✨✨🎇🎇🎉🎉🥂🥂🥂')
